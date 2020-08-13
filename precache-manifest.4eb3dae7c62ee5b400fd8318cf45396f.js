74c51c06712e6f03d479c7ca2b5c0736df563e49
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-13 00:52:22 +0000

    Deploy darrubla/Jobby to github.com/darrubla/Jobby.git:gh-pages

---
+++ precache-manifest.4eb3dae7c62ee5b400fd8318cf45396f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38d5e598d0c3f283372f3f016cde58c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37dd0e759a72689a2555",
+    "url": "./static/css/main.ca4e3155.chunk.css"
+  },
+  {
+    "revision": "5bd2f6f79961924cc520",
+    "url": "./static/js/2.d00b3d6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d00b3d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dd0e759a72689a2555",
+    "url": "./static/js/main.1512a42c.chunk.js"
+  },
+  {
+    "revision": "ab3dbcc6f32fc7f8980c",
+    "url": "./static/js/runtime-main.f90c1032.js"
+  },
+  {
+    "revision": "33a6982a60034f9e19dd3ec493624f0c",
+    "url": "./static/media/building.33a6982a.svg"
+  },
+  {
+    "revision": "2bf72ac7f68a480b7ad1597cf463ff92",
+    "url": "./static/media/location.2bf72ac7.svg"
+  },
+  {
+    "revision": "20146c4822b8361a5ee8df2ebc621b50",
+    "url": "./static/media/money.20146c48.svg"
+  },
+  {
+    "revision": "1a2aba9e86b872e69c40da6d6f237348",
+    "url": "./static/media/target.1a2aba9e.svg"
+  }
+]);
